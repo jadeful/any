a793d2e9ad8154cb675062db04c33f2d45c9ddd4
jadeful <jadeful@yahoo.com>
2019-11-17 21:07:30 +0800

    #4 add

---
+++ src/test/java/br/com/gigio/javarobot/main/com/test/CaptureTest.java
@@ -0,0 +1,26 @@
+package br.com.gigio.javarobot.main.com.test;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class CaptureTest {
+    public static void main(String[] args) {
+        try {
+            Robot robot = new Robot();
+            String format = "jpg";
+            long time = System.currentTimeMillis();
+            String fileName = "shot" + time + format;
+
+            Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+            BufferedImage screenFullImage = robot.createScreenCapture(screenRect);
+            ImageIO.write(screenFullImage, format, new File(fileName));
+
+            System.out.println("A full screenshot captured!");
+        } catch (AWTException | IOException ex) {
+            System.err.println(ex);
+        }
+    }
+}
