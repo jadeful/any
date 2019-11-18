03fc57faeda1901273f063e8dd942950fd2b7bd6
jadeful <jadeful@yahoo.com>
2019-11-18 17:19:24 +0800

    new add

---
+++ src/test/java/br/com/gigio/javarobot/main/com/test/program/Misson.java
@@ -0,0 +1,37 @@
+package br.com.gigio.javarobot.main.com.test.program;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * 要完成的任务
+ */
+public class Misson {
+
+    /**
+     * 唯一主键
+     */
+    Integer id;
+
+    /**
+     * 名称
+     */
+    String name;
+
+    /**
+     *
+     */
+    String date;
+
+    /**
+     * 单个
+     */
+    boolean isSingle;
+
+    /**
+     * 多个
+     */
+    boolean isMulti;
+
+    BufferedImage image;
+
+}
