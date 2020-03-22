0c73e0c41f2d30935494b480d167f306875c0636
jadeful <jadeful@yahoo.com>
2020-03-22 20:29:53 +0800

    #DataStruct add new file

---
+++ src/main/java/datastruct/ArrayQueue.java
@@ -0,0 +1,15 @@
+package datastruct;
+
+/**
+ * 借助数组实现队列
+ */
+public class ArrayQueue {
+
+    private int maxSize;//队列长度
+    private int front;//头
+    private int rear;//尾
+    private int arr[];//队列
+
+
+
+}
---
+++ src/main/java/datastruct/SparseArray.java
@@ -0,0 +1,9 @@
+package datastruct;
+
+/**
+ * 稀疏数组
+ * 应用：五子棋保存棋局；五子棋进入上一局；
+ */
+public class SparseArray {
+    int arr[][]  = new int[11][11];
+}
